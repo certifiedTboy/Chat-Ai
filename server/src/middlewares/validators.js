42d0a970d37198b170ca84640a03bbd4846a7e83
certifiedTboy <etosin70@gmail.com>
2026-07-26 06:34:47 +0100

    add data validator middleware

--- server/src/middlewares/validators.js
+++ server/src/middlewares/validators.js
@@ -1,14 +1,34 @@
-const { validationResult } = require("express-validator");
+const { validationResult, body } = require("express-validator");
+const { HttpException } = require("../config/exception");
 
-const validate = (req, res, next) => {
+const signupWithGoogleValidationRules = [
+  body("authToken")
+    .notEmpty()
+    .withMessage("authentication token is required")
+    .isString()
+    .withMessage("authentication token must be a string"),
+];
+
+const signupWithGithubValidationRules = [
+  body("code")
+    .notEmpty()
+    .withMessage("authentication code is required")
+    .isString()
+    .withMessage("authentication code must be a string"),
+];
+
+const checkValidationResult = (req, _res, next) => {
   const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    return next();
-  }
-  const extractedErrors = [];
-  errors.array().map((err) => extractedErrors.push({ [err.path]: err.msg }));
 
-  return res.status(422).json({ errors: extractedErrors });
+  if (!errors.isEmpty()) {
+    next(new HttpException(400, errors?.array()[0]?.msg || "validation error"));
+  }
+
+  next();
 };
 
-module.exports = { validate };
+module.exports = {
+  checkValidationResult,
+  signupWithGoogleValidationRules,
+  signupWithGithubValidationRules,
+};
--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -6,10 +6,25 @@ const {
   userLoginWithGithub,
 } = require("../controllers/authControllers");
 const Authenticate = require("../middlewares/Authenticate");
+const {
+  checkValidationResult,
+  signupWithGoogleValidationRules,
+  signupWithGithubValidationRules,
+} = require("../middlewares/validators");
 const router = express.Router();
 
-router.post("/google/login", userLoginWithGoogle);
-router.post("/github/login", userLoginWithGithub);
+router.post(
+  "/google/login",
+  signupWithGoogleValidationRules,
+  checkValidationResult,
+  userLoginWithGoogle,
+);
+router.post(
+  "/github/login",
+  signupWithGithubValidationRules,
+  checkValidationResult,
+  userLoginWithGithub,
+);
 router.get("/me", Authenticate, getCurrentUser);
 router.post("/logout", Authenticate, logOutUser);
 
